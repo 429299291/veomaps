84b3788a499ed15603cd698fc458228d7cdd58e0
Enze <enze.zhuang@veoride.com>
2021-08-17 11:45:01 -0500

    fix dashboard apis

--- src/services/dashboard.js
+++ src/services/dashboard.js
@@ -3,25 +3,25 @@ import request from "@/utils/request";
 
 
 export async function getRideCount(params) {
-  return request(`/admins/dashboard/ride_count?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/ride_count?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getCustomerCount(params) {
-  return request(`/admins/dashboard/customer_count?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/customer_count?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getRidePerVehicleRank(params) {
-  return request(`/admins/dashboard/ride_per_vehicle_rank?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/ride_per_vehicle_rank?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getStripeNetCharge(params) {
-  return request(`/admins/dashboard/stripe-net-charge?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/stripe-net-charge?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
@@ -51,38 +51,38 @@ export async function getDailyRideCount(params) {
 }
 
 export async function getWeeklyBatteryState(params) {
-  return request(`/admins/dashboard/weekly_battery_state?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/weekly_battery_state?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getDailyRideRevenue(params) {
-  return request(`/admins/dashboard/daily_ride_revenue?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/daily_ride_revenue?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getStripeDailyRevenue(params) {
-  return request(`/admins/dashboard/stripe_revenue?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/stripe_revenue?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 
 export async function getStripRevenueByPeriod(params) {
-  return request(`/admins/dashboard/stripe_revenue_by_period?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/stripe_revenue_by_period?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getConnectivityByPeriod(params) {
-  return request(`/admins/dashboard/connectivity?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/connectivity?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getAreaTotalMinutes(params) {
-  return request(`/admins/dashboard/total_minutes?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/total_minutes?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
@@ -94,19 +94,19 @@ export async function getAreaTotalMinutes(params) {
 // }
 
 export async function getTotalRideRevenue(params) {
-  return request(`/admins/dashboard/ride_revenue_by_vehicle_type?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/ride_revenue_by_vehicle_type?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getTotalRefund(params) {
-  return request(`/admins/dashboard/total_refund?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/total_refund?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
 
 export async function getPromoSummary(params) {
-  return request(`/admins/dashboard/promo-summary?${stringify(params, { indices: false })}`, {
+  return request(`/api/admins/dashboard/promo-summary?${stringify(params, { indices: false })}`, {
     method: "GET"
   });
 }
