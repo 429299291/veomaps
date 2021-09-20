fda383e96766c78813a583d4dc80914eb54d6752
robot <airobot@robots-MacBook-Pro.local>
2021-09-20 11:18:11 -0500

    fix cicd

---
+++ k8s/package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npm install
+
+if [ "$CI_COMMIT_REF_NAME" == "master" ];
+then
+    sed -i 's/dev/prod/g' src/utils/request.js
+    echo "env is prod"
+else
+    sed -i 's/prod/dev/g' src/utils/request.js
+    echo "env is dev"
+fi
+
+
+npm run build
+ls dist
+sed -i 's/1890ff/51B5AA/g' dist/color.less
