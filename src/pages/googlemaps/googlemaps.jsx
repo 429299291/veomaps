d7305bbb1bc39f10ad12fca3294d9def226ef64a
davis <429299291@qq.com>
2021-12-24 18:25:54 +0800

    googlemaps-success

--- src/pages/googlemaps/googlemaps.jsx
+++ src/pages/googlemaps/googlemaps.jsx
@@ -1,53 +1,70 @@
 import React, { useState, useRef, useCallback,PureComponent } from "react";
 import { LoadScript, GoogleMap, Polygon,InfoWindow } from "@react-google-maps/api";
 import { connect } from "dva";
-
+import {
+  Row,
+  Col,
+  Card,
+  message,
+} from "antd";
 import styles from "./googlemaps.less";
+let allPolygonBuffs = []
 function googlemaps (props){
+  const options = {
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    clickable: true,
+    draggable: false,
+    visible: true,
+    // zIndex: 1
+  };
   const [paths, setPaths] = useState([
-    // [
-    //   {lat:52.52549080781081,lng:13.398178538856462},
-    //   {lat:52.48578559055672,lng:13.36653284549701},
-    //   {lat:52.48871246221605,lng:13.44618372440339},
-    // ],
-  [
-    { lat: 52.52549082781086, lng: 13.398118538856465 },
-    { lat: 52.48578552055679, lng: 13.36653284549709 },
-    { lat: 52.48871242221608, lng: 13.44618372440334 }
-  ],
-  [
-    {lat:52.525,lng:13.380},
-    {lat:52.485,lng:13.36551},
-    {lat:52.4887,lng:13.4461},
-  ]
+  {  
+    name:'fence1',
+    path:[
+      { lat: 52.52549082781086, lng: 13.398118538856465 },
+      { lat: 52.48578552055679, lng: 13.36653284549709 },
+      { lat: 52.48871242221608, lng: 13.44618372440334 }
+    ]
+  },
+  {
+    name:'fence2',
+    path:[
+      {lat:52.425,lng:13.380},
+      {lat:52.485,lng:13.3551},
+      {lat:52.4887,lng:13.4461},
+      {lat:52.4387,lng:13.4161},
+    ]
+  },
 ]);
 const [editableHandler,setEditableHandler] = useState(false)
 const [editIndex,setEditIndex] = useState(null)
-// Define refs for Polygon instance and listeners
+const [clickPolygonReset,setClickPolygonReset] = useState(null)
   const polygonRef = useRef(null);
   const listenersRef = useRef([]);
 
   // Call setPath with new edited path
-  const onEdit = useCallback((index,editableHandler) => {
+  const onEdit = useCallback((index,editIndex,editableHandler) => {
+    polygonRef.current = allPolygonBuffs[index]
+    if(index !== editIndex && editableHandler) return
     if (index === null){
-      console.error('000');
-      // setEditableHandler(false)
-      console.log(polygonRef.current);
+      message.error('check error')
+      setEditableHandler(false)
       return
     }
     setEditableHandler(true)
     if (polygonRef.current) {
-      console.log(polygonRef.current);
       const nextPath = polygonRef.current
         .getPath()
         .getArray()
         .map(latLng => {
           return { lat: latLng.lat(), lng: latLng.lng() };
         });
-        console.log(nextPath);
         let newpaths = [...paths]
-        newpaths[index] = nextPath
-        console.log(newpaths);
+        newpaths[index].path = nextPath
       setPaths(newpaths);
     }
   }, [setPaths]);
@@ -59,47 +76,34 @@ const [editIndex,setEditIndex] = useState(null)
 
   // Bind refs to current Polygon and listeners
   const onLoad = useCallback(
-    polygon => {
-      console.log(polygon);
-      polygonRef.current = polygon;
-      const path = polygon.getPath();
-      console.log(path);
-      listenersRef.current.push(
-        path.addListener("set_at", onEdit),
-        path.addListener("insert_at", onEdit),
-        path.addListener("remove_at", onEdit)
-      );
+    (polygon) => {
+      allPolygonBuffs.push(polygon)
+      setClickPolygonReset(polygon)
     },
     [onEdit]
   );
-  const onLoadInfoWindow =(infoWindow)=>{
-    console.log(infoWindow);
-  }
-  const options = {
-    fillColor: "lightblue",
-    fillOpacity: 0.6,
-    strokeColor: "red",
-    strokeOpacity: 1,
-    strokeWeight: 2,
-    clickable: true,
-    draggable: true,
-    editable: true,
-    geodesic: false,
-    zIndex: 7
-  }
-  const divStyle = {
-    background: `white`,
-    border: `1px solid #ccc`,
-    padding: 15
+  const setActivePolygon =(index)=>{
+    const path = allPolygonBuffs[index].getPath();
+    listenersRef.current.push(
+      path.addListener("set_at", onEdit),
+      path.addListener("insert_at", onEdit),
+      path.addListener("remove_at", onEdit)
+    );
   }
   // Clean up refs
   const onUnmount = useCallback(() => {
+    console.log('unmount');
     listenersRef.current.forEach(lis => lis.remove());
     polygonRef.current = null;
   }, []);
 
-  console.log("The path state is",editIndex, paths);
-  // render(){
+  console.log("The path state is", editIndex ,paths);
+  // infowindow
+  const infowindowOnClose =()=>{
+    if (editableHandler) {
+      setEditableHandler(false);
+    }
+  }
     return (
         <div className={styles.App}>
           <LoadScript
@@ -110,39 +114,42 @@ const [editIndex,setEditIndex] = useState(null)
           >
             <GoogleMap
               mapContainerClassName={styles.Appmap}
-              center={{ lat: 52.52047739093263, lng: 13.36653284549709 }}
+              center={{ lat: 52.47047739093263, lng: 13.36653284549709 }}
               zoom={12}
               version="weekly"
               on
             >
               {
                 paths.map((path,index)=>(
+                  <>
                   <Polygon
                   // Make the Polygon editable / draggable
                   editable = {index == editIndex ? editableHandler :false}
                   draggable={false}
                   key={index}
-                  path={path}
-                  // options={options}
-                  onDblClick={()=>{onEdit((index==editIndex || editIndex == null)?index:null)}}
+                  path={path.path}
+                  options={options}
+                  onClick={() => setActivePolygon(index)}
+                  onDblClick={endEdit}
                   // onMouseOver={}
                   // Event used when manipulating and adding points
                   onMouseUp={()=>{onEdit((index==editIndex || editIndex == null)?index:null);setEditIndex(index)}}
+                  // onMouseUp={onEdit}
                   // Event used when dragging the whole Polygon
                   onDragEnd={()=>{onEdit((index==editIndex || editIndex == null)?index:null)}}
                   onLoad={onLoad}
                   onUnmount={onUnmount}
                 />
+                </>
                 ))
               }
-                 {/* {editableHandler && <InfoWindow
-                  onLoad={onLoadInfoWindow}
-                  position={path[0]}
-                >
-                  <div style={divStyle}>
-                    <h1>InfoWindow1</h1>
+              {editableHandler && editIndex !== null && 
+                <InfoWindow position={paths[editIndex].path[0]} onClose={infowindowOnClose}>
+                  <div>
+                    <span>{paths[editIndex].name}</span>
+                    <br />
                   </div>
-                </InfoWindow>} */}
+                </InfoWindow>}
             </GoogleMap>
           </LoadScript>
         </div>
